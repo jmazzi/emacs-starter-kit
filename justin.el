0683e12d2cf24361166f373ba019b20e0fcba8f7
Justin Mazzi <hh@mailheist.com>
2009-12-02 20:55:03 -0500

    Adding auto-closing

--- justin.el
+++ justin.el
@@ -272,3 +272,16 @@
 (global-set-key "\C-x\C-t" 'twittering-friends-refresh)
 
 (if (file-exists-p "~/.emacs.d/.passwords") (load ".passwords"))
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-hook 'local-write-file-hooks
+                      '(lambda()
+                         (save-excursion
+                           (untabify (point-min) (point-max)))))
+            (set (make-local-variable 'indent-tabs-mode) 'nil)
+            (set (make-local-variable 'tab-width) 2)
+            (define-key ruby-mode-map "\C-m" 'ruby-reindent-then-newline-and-indent)
+            (require 'ruby-electric)
+            (ruby-electric-mode t)))
+
